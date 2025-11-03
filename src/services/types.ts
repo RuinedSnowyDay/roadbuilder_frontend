fa736e97e9db93ab7013938e873f4485c20901d2
Aleksandr Trofimov <atrof002@mit.edu>
2025-11-03 17:13:51 -0500

    fixed semicolon style error

--- src/services/types.ts
+++ src/services/types.ts
@@ -9,52 +9,63 @@ export interface Session {
 }
 
 export interface AssignedObject {
-  _id: string
-  owner: string
-  object: string
-  title: string
-  description: string
+  _id: string;
+  owner: string;
+  object: string;
+  title: string;
+  description: string;
+}
+
+export interface CreateAssignedObjectResponse {
+  assignedObject: string;
 }
 
 export interface Graph {
-  _id: string
-  owner: string
-  title: string
+  _id: string;
+  owner: string;
+  title: string;
+}
+
+export interface GraphResponse {
+  newGraph: string;
+}
+
+export interface AccessGraphResponse {
+  accessedGraph: string;
 }
 
 export interface Node {
-  _id: string
-  parent: string // Graph ID
-  title: string
-  enrichment: string // Object ID
+  _id: string;
+  parent: string; // Graph ID
+  title: string;
+  enrichment: string; // Object ID
 }
 
 export interface Edge {
-  _id: string
-  source: string // Node ID
-  target: string // Node ID
-  enrichment: string // Object ID
+  _id: string;
+  source: string; // Node ID
+  target: string; // Node ID
+  enrichment: string; // Object ID
 }
 
 export interface ResourceList {
-  _id: string
-  title: string
-  owner: string
-  length: number
+  _id: string;
+  title: string;
+  owner: string;
+  length: number;
 }
 
 export interface IndexedResource {
-  _id: string
-  resource: string
-  title: string
-  list: string
-  index: number
+  _id: string;
+  resource: string;
+  title: string;
+  list: string;
+  index: number;
 }
 
 export interface Check {
-  _id: string
-  user: string
-  object: string
-  checked: boolean
+  _id: string;
+  user: string;
+  object: string;
+  checked: boolean;
 }
-
