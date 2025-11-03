0f43ff1fc6960588cbc844fe45094598b0eb254e
Aleksandr Trofimov <atrof002@mit.edu>
2025-11-03 17:35:05 -0500

    added protected route to access the roadmap view

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,6 +22,12 @@ const router = createRouter({
       component: () => import('../views/RegisterView.vue'),
       meta: { requiresGuest: true },
     },
+    {
+      path: '/roadmap/:id',
+      name: 'roadmap',
+      component: () => import('../views/RoadmapView.vue'),
+      meta: { requiresAuth: true },
+    },
   ],
 })
 
